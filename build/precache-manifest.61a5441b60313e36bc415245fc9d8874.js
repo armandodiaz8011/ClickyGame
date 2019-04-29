33d7e31583ff2262cf488bcd559fba25b1583320
Armando Diaz <armandodiaz8011@armandos-mbp.attlocal.net>
2019-04-29 15:51:37 -0400

    Heroku

---
+++ build/precache-manifest.61a5441b60313e36bc415245fc9d8874.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "726a010c8b7db0562d39",
+    "url": "/static/js/main.ad0df0e2.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "726a010c8b7db0562d39",
+    "url": "/static/css/main.d4624def.chunk.css"
+  },
+  {
+    "revision": "81a697f49a089cd26223baa0d06dd557",
+    "url": "/index.html"
+  }
+];
